5a9a14f5e37151d3f199d420094c85b4c7d98514
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-08-20 11:25:52 -0300

    Brincando com funções e mexendo no geral

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const botaoCalcular = document.getElementById('botao')
+        const mostrarResultado = document.getElementById('resultado')
+
+        function calcularSoma(){
+            let v1 = parseInt(document.getElementById('n1').value);
+            let v2 = parseInt(document.getElementById('n2').value);
+
+            let total = v1 + v2;
+
+            document.getElementById('resultado').innerHTML = total;
+        }
